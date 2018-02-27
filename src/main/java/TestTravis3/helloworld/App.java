6981db521fc53c20adc1e124b129f1cbd4088b7f
syd-baroya <sbaroya@calpoly.edu>
2018-02-27 11:57:59 -0800

    adding helloworld files

---
+++ src/main/java/TestTravis3/helloworld/App.java
@@ -0,0 +1,13 @@
+package TestTravis3.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
